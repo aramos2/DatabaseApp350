2a4df1dd7f5306f06c1516003f68d23488a4716c
aramos2 <44303370+aramos2@users.noreply.github.com>
2020-10-04 16:35:53 -0400

    Update server.js

--- workshop-server/workshop-server/server.js
+++ workshop-server/workshop-server/server.js
@@ -1,9 +1,25 @@
+require('dotenv').config()
+
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 
-app.set("port", 8080);
+app.set("port", 8080); 
+
+app.use(bodyParser.json({ type: "application/json" }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const Pool = require("pg").Pool;
+const config = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: "workshop"
+};
+
+// Greeting the USER 
 
 app.get('/hello', (req, res) => {
   // console log the request query json object
@@ -15,7 +31,31 @@ app.get('/hello', (req, res) => {
 });
 
 
+// get names of attendees from a workshop if the workshop search is not in the database return a list of workshops available. 
 
+
+
+
+
+
+
+
+
+
+/* Add attendees to the workshop database that contains two agruements: 
+attendees and workshop 
+if either or both arguments are missing return an error 
+if the attendee is already the workshop return an error
+if all requires are met, the attendees name and workshop they signed up for should be returned. 
+*/ 
+
+
+
+
+
+
+
+// SERVER START
 app.listen(app.get("port"), () => {
   console.log(`Find the server at: http://localhost:${app.get("port")}/`); 
 });
